9db951459f4a7761214c7a7dae6b4226991ce51d
Guillaume Delmas-Frenette <guillaume.delmas-frenette@mail.mcgill.ca>
2021-10-12 14:15:33 -0400

    Testing creation of branch

--- OnlineLibrary-Backend/src/main/java/ca/mcgill/ecse321/onlinelibrary/model/Loan.java
+++ OnlineLibrary-Backend/src/main/java/ca/mcgill/ecse321/onlinelibrary/model/Loan.java
@@ -1,5 +1,16 @@
 package ca.mcgill.ecse321.onlinelibrary.model;
 
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+import java.sql.Date;
+import java.sql.Time;
+
 public class Loan {
 
 }
