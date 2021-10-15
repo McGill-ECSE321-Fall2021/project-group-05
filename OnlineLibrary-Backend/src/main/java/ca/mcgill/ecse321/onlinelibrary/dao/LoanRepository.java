17e33a80273b3a2e05689bc7e73c138052a23a29
Gabriel Lacroix <lacroixgabriel@gmail.com>
2021-10-15 14:24:10 -0400

    Move LoanRepository to new branch

--- OnlineLibrary-Backend/src/main/java/ca/mcgill/ecse321/onlinelibrary/dao/LoanRepository.java
+++
@@ -1,8 +0,0 @@
-//package ca.mcgill.ecse321.onlinelibrary.dao;
-//
-//import org.springframework.data.repository.CrudRepository;
-//import ca.mcgill.ecse321.onlinelibrary.model.Loan;
-//
-//public interface LoanRepository extends CrudRepository<Loan, Integer>{
-//	Loan findLoanById(Integer id);
-//}
