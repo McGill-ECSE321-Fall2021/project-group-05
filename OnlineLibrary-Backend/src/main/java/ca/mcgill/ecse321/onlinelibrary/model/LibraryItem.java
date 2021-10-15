d16cd97e2c28f49ffe5b0879d4b6625187c057e7
Guillaume Delmas-Frenette <guillaume.delmas-frenette@mail.mcgill.ca>
2021-10-15 14:35:12 -0400

    Deleted inheritance strategy annotation

--- OnlineLibrary-Backend/src/main/java/ca/mcgill/ecse321/onlinelibrary/model/LibraryItem.java
+++ OnlineLibrary-Backend/src/main/java/ca/mcgill/ecse321/onlinelibrary/model/LibraryItem.java
@@ -3,7 +3,6 @@ package ca.mcgill.ecse321.onlinelibrary.model;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public abstract class LibraryItem {
 
     @Id
