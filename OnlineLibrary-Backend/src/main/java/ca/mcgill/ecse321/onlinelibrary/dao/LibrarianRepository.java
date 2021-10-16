850521ff766fc3794ee1abb8d7cc2875f7b0693c
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2021-10-16 18:42:42 -0400

    Create LibrarianRepository

---
+++ OnlineLibrary-Backend/src/main/java/ca/mcgill/ecse321/onlinelibrary/dao/LibrarianRepository.java
@@ -0,0 +1,9 @@
+package ca.mcgill.ecse321.onlinelibrary.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import ca.mcgill.ecse321.onlinelibrary.model.Librarian;
+
+public interface LibrarianRepository extends CrudRepository<Librarian, Integer> {
+
+	public Librarian findLibrarianById(int id);
+}
