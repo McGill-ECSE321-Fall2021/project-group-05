d5fa6c44cfa2881f7218943eea15e479b7dbef04
SMI3 <sebastian_ionascu@hotmail.com>
2021-10-17 19:15:54 -0400

    final changes in reservable item info before merge with main

--- OnlineLibrary-Backend/src/main/java/ca/mcgill/ecse321/onlinelibrary/model/ReservableItemInfo.java
+++ OnlineLibrary-Backend/src/main/java/ca/mcgill/ecse321/onlinelibrary/model/ReservableItemInfo.java
@@ -1,10 +1,9 @@
 package ca.mcgill.ecse321.onlinelibrary.model;
 
-import java.util.*;
+//import java.util.*;
 import javax.persistence.*;
 
 @Entity
-
 public abstract class ReservableItemInfo extends LibraryItemInfo {
 	// // Seb : is this actually useful? Louis needs it?
 	// @ManyToMany
