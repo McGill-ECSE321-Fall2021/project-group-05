6d5aafa6f8eec07ae49f624267374b5df599ac0d
Gabriel Lacroix <lacroixgabriel@gmail.com>
2021-10-18 19:47:51 -0400

    Make info classes not optional for copy classes

--- OnlineLibrary-Backend/src/main/java/ca/mcgill/ecse321/onlinelibrary/model/Archive.java
+++ OnlineLibrary-Backend/src/main/java/ca/mcgill/ecse321/onlinelibrary/model/Archive.java
@@ -1,11 +1,15 @@
 package ca.mcgill.ecse321.onlinelibrary.model;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 
 @Entity
 public class Archive extends LibraryItem{
-    @ManyToOne
+    @ManyToOne(optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private ArchiveInfo archiveInfo;
 
     public Archive(ArchiveInfo archiveInfo) {
