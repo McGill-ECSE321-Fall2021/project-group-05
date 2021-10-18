6cb85ea37ce7e4beca53bb11792a3168331fc5b5
Gabriel Lacroix <lacroixgabriel@gmail.com>
2021-10-17 20:09:41 -0400

    Add doc comments to methods in model classes

--- OnlineLibrary-Backend/src/main/java/ca/mcgill/ecse321/onlinelibrary/model/ReservableItem.java
+++ OnlineLibrary-Backend/src/main/java/ca/mcgill/ecse321/onlinelibrary/model/ReservableItem.java
@@ -10,10 +10,20 @@ public abstract class ReservableItem extends LibraryItem {
 
     private ItemStatus status;
 
+    /**
+     * Returns the status of this reservable library item copy.
+     *
+     * The status could be any of Available, CheckedOut and Reserved.
+     * @return the current status of this reservable library item
+     */
     public ItemStatus getStatus() {
         return this.status;
     }
 
+    /**
+     * Sets the new status of this reservable library item copy.
+     * @param status the new status of this reservable item
+     */
     public void setStatus(ItemStatus status) {
         this.status = status;
     }
@@ -21,10 +31,19 @@ public abstract class ReservableItem extends LibraryItem {
     @OneToOne(cascade = { CascadeType.ALL })
 	@JoinColumn(name = "loan_id")
     private Loan loan;
+
+    /**
+     * Get the current loan associated with this item if there is one.
+     * @return the current loan associated with this item
+     */
     public Loan getLoan() {
     	return this.loan;
     }
-    
+
+    /**
+     * Associate a new loan with the item.
+     * @param loan the new loan too associate with the reservable library item
+     */
     public void setLoan(Loan loan) {
     	this.loan = loan;
     }
