927dadf50e56c0ce81459e299d7f74ba1c511d1c
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2021-11-05 15:02:55 -0400

    Replace PostMapping with DeleteMapping

--- OnlineLibrary-Backend/src/main/java/ca/mcgill/ecse321/onlinelibrary/controller/LibrarianController.java
+++ OnlineLibrary-Backend/src/main/java/ca/mcgill/ecse321/onlinelibrary/controller/LibrarianController.java
@@ -2,8 +2,8 @@ package ca.mcgill.ecse321.onlinelibrary.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,12 +16,12 @@ public class LibrarianController {
 	@Autowired
 	private LibrarianService librarianService;
 
-	@PostMapping(value = {"/librarian/delete/{id}", "/librarian/delete/{id}/"})
+	@DeleteMapping(value = {"/librarian/delete/{id}", "/librarian/delete/{id}/"})
 	public void deleteLibrarianById(@PathVariable("id") int id) {
 		librarianService.deleteLibrarianById(id);
 	}
 
-	@PostMapping(value = {"/librarian/delete", "/librarian/delete"})
+	@DeleteMapping(value = {"/librarian/delete", "/librarian/delete"})
 	public void deleteLibrarianByUsername(@RequestParam String username) {
 		librarianService.deleteLibrarianByUsername(username);
 	}
