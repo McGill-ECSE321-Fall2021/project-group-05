ed96db9d34f6008bb9994239c7be8c55bc1c47b3
Mircea Gosman <mirceagosman@gmail.com>
2021-11-04 21:09:53 -0400

    Added Date-driven queries for holidays & opening hours
    The front-end is more likely to query by date ranges instead of by ID. These additional methods should simplify future development.
    
    Relevant tests have also been added.

--- OnlineLibrary-Backend/src/main/java/ca/mcgill/ecse321/onlinelibrary/dao/LibraryOpeningHoursRepository.java
+++ OnlineLibrary-Backend/src/main/java/ca/mcgill/ecse321/onlinelibrary/dao/LibraryOpeningHoursRepository.java
@@ -3,6 +3,8 @@ package ca.mcgill.ecse321.onlinelibrary.dao;
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.onlinelibrary.model.LibraryOpeningHours;
+import java.sql.Date;
+import java.util.ArrayList;
 
 public interface LibraryOpeningHoursRepository extends CrudRepository<LibraryOpeningHours, Integer> {
 	/**
@@ -11,4 +13,25 @@ public interface LibraryOpeningHoursRepository extends CrudRepository<LibraryOpe
 	 * @return the library opening hours instance
 	 */
 	LibraryOpeningHours findLibraryOpeningHoursById(int id);
+	
+	/**
+	 * Finds library opening hours at a particular date  
+	 * @param date of the library opening hours
+	 * @return ArrayList of library opening hours at the date
+	 */
+	ArrayList<LibraryOpeningHours> findLibraryOpeningHoursByDate(Date date);
+	
+	/**
+	 * Finds library opening hours contained in a date range 
+	 * @param startDate of the date range
+	 * @param endDate of the date range
+	 * @return ArrayList of library opening hours in the range
+	 */
+	ArrayList<LibraryOpeningHours> findLibraryOpeningHoursByDateBetween(Date startDate, Date endDate);
+	
+	/**
+	 * Deletes library opening hours by id
+	 * @param unique ID of the library opening hours 
+	 */
+	void deleteById(int id);
 }
