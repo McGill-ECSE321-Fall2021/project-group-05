f996547cb2e58f8849a930783b0aa3c6cda67a37
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2021-11-06 15:58:11 -0400

    Add existsMemberByUsername() method to MemberRepository

--- OnlineLibrary-Backend/src/main/java/ca/mcgill/ecse321/onlinelibrary/dao/MemberRepository.java
+++ OnlineLibrary-Backend/src/main/java/ca/mcgill/ecse321/onlinelibrary/dao/MemberRepository.java
@@ -1,11 +1,11 @@
 package ca.mcgill.ecse321.onlinelibrary.dao;
 
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.onlinelibrary.model.Member;
 
 public interface MemberRepository extends CrudRepository<Member, Integer> {
+
 	public Member findMemberById(int id);
 
+	public boolean existsMemberByUsername(String username);
 }
