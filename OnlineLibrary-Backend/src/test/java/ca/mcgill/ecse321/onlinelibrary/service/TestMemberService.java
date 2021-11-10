cc5c0027d48f903be5f8376b123998de4496e85e
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2021-11-10 14:39:58 -0500

    Write test for successfully creating new online account

--- OnlineLibrary-Backend/src/test/java/ca/mcgill/ecse321/onlinelibrary/service/TestMemberService.java
+++ OnlineLibrary-Backend/src/test/java/ca/mcgill/ecse321/onlinelibrary/service/TestMemberService.java
@@ -7,7 +7,10 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -39,13 +42,21 @@ public class TestMemberService {
 	private final int REGISTRATION_FEE = 1000;
 	private final int MIN_PASSWD_LENGTH = 8;
 
-	// Existing member
+	// Existing member with online account
 	private static final int OLD_MEMBER_ID = 42;
 	private static final String OLD_MEMBER_FULL_NAME = "John Doe";
 	private static final String OLD_MEMBER_ADDRESS = "123 Main Street";
 	private static final String OLD_MEMBER_USERNAME = "john.doe";
 	private static final String OLD_MEMBER_EMAIL_ADDRESS = "john.doe@gmail.com";
 	private final String OLD_MEMBER_PASSWD = "m".repeat(MIN_PASSWD_LENGTH);
+
+	// Existing member without online account (but who wishes to create one)
+	private static final int OFFLINE_MEMBER_ID = 43;
+	private static final String OFFLINE_MEMBER_FULL_NAME = "Yoda";
+	private static final String OFFLINE_MEMBER_ADDRESS = "900 McGill Street";
+	private static final String OFFLINE_MEMBER_USERNAME = "yoda900";
+	private static final String OFFLINE_MEMBER_EMAIL_ADDRESS = "yoda@mail.mcgill.ca";
+	private final String OFFLINE_MEMBER_PASSWD = "y".repeat(MIN_PASSWD_LENGTH);
 
 	// New member
 	private static final String NEW_MEMBER_FULL_NAME = "Obi-Wan Kenobi";
@@ -60,10 +71,17 @@ public class TestMemberService {
 	private final String SHORT_PASSWD = "p".repeat(MIN_PASSWD_LENGTH - 1);
 
 	/**
-	 * Assume the database already contains one inactive member with ID
-	 * MEMBER_ID, full name MEMBER_FULL_NAME, address MEMBER_ADDRESS, username
-	 * MEMBER_USERNAME, email address MEMBER_EMAIL_ADDRESS, and password
-	 * MEMBER_PASSWD.
+	 * Assume the database already contains
+	 *
+	 * <ul>
+	 * <li>one inactive member with ID OLD_MEMBER_ID, full name
+	 * OLD_MEMBER_FULL_NAME, address OLD_MEMBER_ADDRESS, username
+	 * OLD_MEMBER_USERNAME, email address OLD_MEMBER_EMAIL_ADDRESS, and password
+	 * OLD_MEMBER_PASSWD
+	 * <li>one inactive member with ID OFFLINE_MEMBER_ID, full name
+	 * OFFLINE_MEMBER_FULL_NAME, address OFFLINE_MEMBER_ADDRESS, and no online
+	 * account
+	 * </ul>
 	 */
 	@BeforeEach
 	public void setMockOuput() {
@@ -73,6 +91,9 @@ public class TestMemberService {
 				OnlineAccount onlineAccount = new OnlineAccount(OLD_MEMBER_PASSWD, OLD_MEMBER_USERNAME,
 						OLD_MEMBER_EMAIL_ADDRESS, member);
 				member.setOnlineAccount(onlineAccount);
+				return member;
+			} else if (OFFLINE_MEMBER_ID == (int) invocation.getArgument(0)) {
+				Member member = new Member(OFFLINE_MEMBER_ADDRESS, OFFLINE_MEMBER_FULL_NAME);
 				return member;
 			} else {
 				return null;
@@ -330,5 +351,25 @@ public class TestMemberService {
 		Exception error = assertThrows(IllegalArgumentException.class, () -> memberService.registerMember(memberDto));
 		assertContains("Email address cannot be empty.", error.getMessage());
 		assertContains("Full name cannot be empty.", error.getMessage());
+	}
+
+	@Test
+	public void testCreateOnlineAccount() {
+		CreateOnlineAccountRequestDto newOnlineAccount = new CreateOnlineAccountRequestDto(
+				"  " + OFFLINE_MEMBER_USERNAME + "  ", "  " + OFFLINE_MEMBER_EMAIL_ADDRESS + "  ",
+				"  " + OFFLINE_MEMBER_PASSWD + "  ");
+
+		OnlineAccount createdAccount = memberService.createOnlineAccount(OFFLINE_MEMBER_ID, newOnlineAccount);
+
+		// Check properties of returned online account
+		assertNotNull(createdAccount);
+		assertEquals(OFFLINE_MEMBER_USERNAME, createdAccount.getUsername());
+		assertEquals(OFFLINE_MEMBER_EMAIL_ADDRESS, createdAccount.getEmailAddress());
+		// TODO Update this if/when password hashing is implemented
+		assertEquals(OFFLINE_MEMBER_PASSWD, createdAccount.getPasswordHash());
+
+		// Check that online account was saved to database
+		verify(memberDao, times(1)).save(argThat((Member m) -> m.getOnlineAccount() != null
+				&& OFFLINE_MEMBER_USERNAME.equals(m.getOnlineAccount().getUsername())));
 	}
 }
