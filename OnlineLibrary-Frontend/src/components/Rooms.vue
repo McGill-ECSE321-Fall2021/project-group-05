b4ebbdf660e4805350560133c232d0e083dc8dba
Gabriel Lacroix <lacroixgabriel@gmail.com>
2021-11-18 19:06:41 -0500

    Create Rooms component

---
+++ OnlineLibrary-Frontend/src/components/Rooms.vue
@@ -0,0 +1,54 @@
+<template>
+  <main>
+    <Header />
+    <h2>Rooms</h2>
+    <table>
+      <tr>
+        <th>Room ID</th>
+        <th>Room Name</th>
+        <th>Room Capacity</th>
+      </tr>
+      <tr v-for="room in rooms" :key="room.id">
+        <td>{{ room.id }}</td>
+        <td>{{ room.name }}</td>
+        <td>{{ room.capacity }}</td>
+      </tr>
+    </table>
+  </main>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import axios from "axios";
+const config = require("../../config");
+
+const frontendUrl = "http://" + config.build.host + ":" + config.build.port;
+const backendUrl =
+  "http://" + config.build.backendHost + ":" + config.build.backendPort;
+
+const axios_instance = axios.create({
+  baseURL: backendUrl,
+  headers: { "Access-Control-Allow-Origin": frontendUrl }
+});
+
+export default {
+  name: "Rooms",
+  components: {
+    Header
+  },
+  created: function() {
+    axios_instance
+      .get("/room/")
+      .then(response => {
+        this.rooms = response.data;
+        console.log(response.data);
+      })
+      .catch(error => console.log(error));
+  },
+  data() {
+    return {
+      rooms: []
+    };
+  }
+};
+</script>
