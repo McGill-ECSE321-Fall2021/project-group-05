49ada478bf5cb32873e5a69a6a5b4fd543aff9b0
Gabriel Lacroix <lacroixgabriel@gmail.com>
2021-11-18 19:06:19 -0500

    Create Header component

---
+++ OnlineLibrary-Frontend/src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><router-link :to="{ name: 'Rooms' }">Browse rooms</router-link></li>
+        <li><router-link :to="{ name: 'Hello' }">Welcome page</router-link></li>
+        <li><a>Log out</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style>
+header nav ul {
+  background-color: #000;
+  height: 100px;
+}
+nav ul {
+  display: flex;
+}
+nav ul li {
+  font-size: 20px;
+  text-align: center;
+  padding: 20px 50px;
+  justify-content: center;
+  list-style-type: none;
+  align-self: center;
+}
+nav ul li a {
+  color: #fff;
+  text-decoration: none;
+}
+nav ul li a:hover {
+  color: #fff;
+  text-decoration-style: dotted;
+}
+</style>
