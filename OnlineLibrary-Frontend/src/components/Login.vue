f4a1c50de20261b33c5aa05d09c3146454cd46b2
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2021-11-24 15:18:27 -0500

    Implement coding conventions in Login component

--- OnlineLibrary-Frontend/src/components/Login.vue
+++ OnlineLibrary-Frontend/src/components/Login.vue
@@ -1,93 +1,95 @@
 <template>
-  <div class="login">
+  <body>
     <h1>Welcome to the online library system!</h1>
-    <h2>Log in to your online account:</h2>
-    <b-container>
-      <b-row>
-        <b-col>
-          <h2>Member</h2>
-          <b-form>
+    <main>
+      <h2>Log in to your online account:</h2>
+      <b-container>
+        <b-row>
+          <b-col>
+            <h2>Member</h2>
+            <b-form>
+              <b-row>
+                <!-- Disallow spaces in username -->
+                <input
+                  type="text"
+                  @keydown.space.prevent
+                  placeholder="Username"
+                  v-model="memberUsername"
+                />
+              </b-row>
+              <b-row>
+                <!-- Disallow spaces in password -->
+                <input
+                  type="password"
+                  @keydown.space.prevent
+                  placeholder="Password"
+                  v-model="memberPassword"
+                />
+              </b-row>
+              <b-row>
+                <button
+                  v-bind:disabled="
+                    !memberUsername.trim() || !memberPassword.trim()
+                  "
+                  v-on:click="doLogInMember(memberUsername, memberPassword)"
+                >
+                  Log in
+                </button>
+              </b-row>
+            </b-form>
             <b-row>
-              <!-- Disallow spaces in username -->
-              <input
-                type="text"
-                @keydown.space.prevent
-                placeholder="Username"
-                v-model="memberUsername"
-              />
+              <span v-if="memberErrorMsg" class="errorMsg">
+                Error: {{ memberErrorMsg }}
+              </span>
             </b-row>
+          </b-col>
+          <b-col>
+            <h2>Employee</h2>
+            <b-form>
+              <b-row>
+                <!-- Disallow spaces in username -->
+                <input
+                  type="username"
+                  @keydown.space.prevent
+                  placeholder="Username"
+                  v-model="librarianUsername"
+                />
+              </b-row>
+              <b-row>
+                <!-- Disallow spaces in password -->
+                <input
+                  type="password"
+                  @keydown.space.prevent
+                  placeholder="Password"
+                  v-model="librarianPassword"
+                />
+              </b-row>
+              <b-row>
+                <button
+                  v-bind:disabled="
+                    !librarianUsername.trim() || !librarianPassword.trim()
+                  "
+                  v-on:click="
+                    doLogInLibrarian(librarianUsername, librarianPassword)
+                  "
+                >
+                  Log in
+                </button>
+              </b-row>
+            </b-form>
             <b-row>
-              <!-- Disallow spaces in password -->
-              <input
-                type="password"
-                @keydown.space.prevent
-                placeholder="Password"
-                v-model="memberPassword"
-              />
+              <span v-if="librarianErrorMsg" class="errorMsg">
+                Error: {{ librarianErrorMsg }}
+              </span>
             </b-row>
-            <b-row>
-              <button
-                v-bind:disabled="
-                  !memberUsername.trim() || !memberPassword.trim()
-                "
-                v-on:click="doLogInMember(memberUsername, memberPassword)"
-              >
-                Log in
-              </button>
-            </b-row>
-          </b-form>
-          <b-row>
-            <span v-if="memberErrorMsg" class="errorMsg">
-              Error: {{ memberErrorMsg }}
-            </span>
-          </b-row>
-        </b-col>
-        <b-col>
-          <h2>Employee</h2>
-          <b-form>
-            <b-row>
-              <!-- Disallow spaces in username -->
-              <input
-                type="username"
-                @keydown.space.prevent
-                placeholder="Username"
-                v-model="librarianUsername"
-              />
-            </b-row>
-            <b-row>
-              <!-- Disallow spaces in password -->
-              <input
-                type="password"
-                @keydown.space.prevent
-                placeholder="Password"
-                v-model="librarianPassword"
-              />
-            </b-row>
-            <b-row>
-              <button
-                v-bind:disabled="
-                  !librarianUsername.trim() || !librarianPassword.trim()
-                "
-                v-on:click="
-                  doLogInLibrarian(librarianUsername, librarianPassword)
-                "
-              >
-                Log in
-              </button>
-            </b-row>
-          </b-form>
-          <b-row>
-            <span v-if="librarianErrorMsg" class="errorMsg">
-              Error: {{ librarianErrorMsg }}
-            </span>
-          </b-row>
-        </b-col>
-      </b-row>
-    </b-container>
-    <p>
-      Don't have an account? <router-link to="/signup">Sign up</router-link>
-    </p>
-  </div>
+          </b-col>
+        </b-row>
+      </b-container>
+      <p>
+        Don't have an account? <router-link to="/signup">Sign up</router-link>
+      </p>
+    </main>
+  </body>
 </template>
 
 <script src="./login.js">
--- OnlineLibrary-Frontend/src/components/login.js
+++ OnlineLibrary-Frontend/src/components/login.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-var config = require('../../config')
+const config = require('../../config');
 
 const backendUrl = (process.env.NODE_ENV === "production")
   ? `http://${config.build.backendHost}:${config.build.backendPort}`
@@ -8,10 +8,10 @@ const backendUrl = (process.env.NODE_ENV === "production")
 const frontendUrl = (process.env.NODE_ENV === "production")
   ? `http://${config.build.host}:${config.build.port}`
   : `http://${config.dev.host}:${config.dev.port}`;
-var AXIOS = axios.create({
+const AXIOS = axios.create({
   baseURL: backendUrl,
   headers: { 'Access-Control-Allow-Origin': frontendUrl }
-})
+});
 
 export default {
   name: 'login',
@@ -23,58 +23,60 @@ export default {
       librarianUsername: '',
       librarianPassword: '',
       librarianErrorMsg: ''
-    }
+    };
   },
   methods: {
-    doLogInMember: function (username, password) {
-      var self = this;
+    doLogInMember(username, password) {
+      const self = this;
       AXIOS.get('/member/login', {
         params: {
           username,
           password
         }
       })
-        .then(function (response) {
+        .then((response) => {
           // Store stringified member info in session storage and redirect to member home page
           if (response.status === 200) {
-            sessionStorage.setItem("loggedInMember", JSON.stringify(response.data))
+            sessionStorage.setItem("loggedInMember", JSON.stringify(response.data));
             // Prevent 2 users from being logged in at once
-            sessionStorage.removeItem("loggedInLibrarian")
-            // TODO: Get url of member home page
-            window.location.href = "/#/home/member"
+            sessionStorage.removeItem("loggedInLibrarian");
+            self.$router.push("/home/member");
           }
           // TODO: Do actual error handling
           else {
-            self.memberErrorMsg = "Invalid username or password."
+            console.log("Response status: " + response.status.toString());
+            self.memberErrorMsg = "Invalid username or password.";
           }
         })
         // TODO: Do actual error handling
-        .catch(error => self.memberErrorMsg = "Invalid username or password.")
+        .catch((error) => {
+          console.log(error);
+          self.memberErrorMsg = "Invalid username or password.";
+        })
     },
     doLogInLibrarian(username, password) {
-      var self = this;
+      const self = this;
       AXIOS.get('/librarian/login', {
         params: {
           username,
           password
         }
       })
-        .then(function (response) {
+        .then((response) => {
           // Store stringified member info in session storage and redirect to member home page
           if (response.status === 200) {
-            sessionStorage.setItem("loggedInLibrarian", JSON.stringify(response.data))
+            sessionStorage.setItem("loggedInLibrarian", JSON.stringify(response.data));
             // Prevent 2 users from being logged in at once
-            sessionStorage.removeItem("loggedInMember")
-            // TODO: Get url of librarian home page
-            window.location.href = "/#/home/librarian"
+            sessionStorage.removeItem("loggedInMember");
+            self.$router.push("/home/librarian");
           }
           // TODO: Do actual error handling
           else {
-            self.librarianErrorMsg = "Invalid username or password."
+            self.librarianErrorMsg = "Invalid username or password.";
           }
         })
         // TODO: Do actual error handling
-        .catch(error => self.librarianErrorMsg = "Invalid username or password.")
+        .catch((error) => self.librarianErrorMsg = "Invalid username or password.")
     }
   }
 }
