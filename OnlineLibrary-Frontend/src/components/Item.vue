7af5b96df28afc12bc17b4dd7196bd147635b702
Gabriel Lacroix <lacroixgabriel@gmail.com>
2021-11-24 15:54:50 -0500

    Add category icons to the item view

--- OnlineLibrary-Frontend/src/components/Item.vue
+++ OnlineLibrary-Frontend/src/components/Item.vue
@@ -5,80 +5,95 @@
       <h1>Book</h1>
       <h2>{{ this.item.title }}</h2>
       <hr />
-      <dl>
-        <dt>Id</dt>
-        <dd>{{ this.item.id }}</dd>
-        <dt>Title</dt>
-        <dd>{{ this.item.title }}</dd>
-        <dt>Author</dt>
-        <dd>{{ this.item.author }}</dd>
-        <dt>Number of pages</dt>
-        <dd>{{ this.item.numberOfPage }}</dd>
-        <dt>ISBN</dt>
-        <dd>{{ this.item.isbn }}</dd>
-      </dl>
+      <div class="item-description-container">
+        <img src="../assets/book.svg" alt="Book cover" />
+        <dl>
+          <dt>Id</dt>
+          <dd>{{ this.item.id }}</dd>
+          <dt>Title</dt>
+          <dd>{{ this.item.title }}</dd>
+          <dt>Author</dt>
+          <dd>{{ this.item.author }}</dd>
+          <dt>Number of pages</dt>
+          <dd>{{ this.item.numberOfPage }}</dd>
+          <dt>ISBN</dt>
+          <dd>{{ this.item.isbn }}</dd>
+        </dl>
+      </div>
     </main>
     <main v-else-if="this.item.type === 'Album'">
       <h1>Album</h1>
       <h2>{{ this.item.title }}</h2>
       <hr />
-      <dl>
-        <dt>Id</dt>
-        <dd>{{ this.item.id }}</dd>
-        <dt>Title</dt>
-        <dd>{{ this.item.title }}</dd>
-        <dt>Composer/performer</dt>
-        <dd>{{ this.item.composerPerformer }}</dd>
-        <dt>Genre</dt>
-        <dd>{{ this.item.genre }}</dd>
-      </dl>
+      <div class="item-description-container">
+        <img src="../assets/album.svg" alt="Album cover" />
+        <dl>
+          <dt>Id</dt>
+          <dd>{{ this.item.id }}</dd>
+          <dt>Title</dt>
+          <dd>{{ this.item.title }}</dd>
+          <dt>Composer/performer</dt>
+          <dd>{{ this.item.composerPerformer }}</dd>
+          <dt>Genre</dt>
+          <dd>{{ this.item.genre }}</dd>
+        </dl>
+      </div>
     </main>
     <main v-else-if="this.item.type === 'Archive'">
       <h1>Archive</h1>
       <h2>{{ this.item.title }}</h2>
       <hr />
-      <dl>
-        <dt>Id</dt>
-        <dd>{{ this.item.id }}</dd>
-        <dt>Title</dt>
-        <dd>{{ this.item.title }}</dd>
-        <dt>Description</dt>
-        <dd>{{ this.item.description }}</dd>
-      </dl>
+      <div class="item-description-container">
+        <img src="../assets/archive.svg" alt="Archive box" />
+        <dl>
+          <dt>Id</dt>
+          <dd>{{ this.item.id }}</dd>
+          <dt>Title</dt>
+          <dd>{{ this.item.title }}</dd>
+          <dt>Description</dt>
+          <dd>{{ this.item.description }}</dd>
+        </dl>
+      </div>
     </main>
     <main v-else-if="this.item.type === 'Movie'">
       <h1>Movie</h1>
       <h2>{{ this.item.title }}</h2>
       <hr />
-      <dl>
-        <dt>Id</dt>
-        <dd>{{ this.item.id }}</dd>
-        <dt>Title</dt>
-        <dd>{{ this.item.title }}</dd>
-        <dt>Director</dt>
-        <dd>{{ this.item.director }}</dd>
-        <dt>Genre</dt>
-        <dd>{{ this.item.genre }}</dd>
-        <dt>Duration</dt>
-        <dd>{{ this.item.length }}</dd>
-      </dl>
+      <div class="item-description-container">
+        <img src="../assets/movie.svg" alt="Movie icon" />
+        <dl>
+          <dt>Id</dt>
+          <dd>{{ this.item.id }}</dd>
+          <dt>Title</dt>
+          <dd>{{ this.item.title }}</dd>
+          <dt>Director</dt>
+          <dd>{{ this.item.director }}</dd>
+          <dt>Genre</dt>
+          <dd>{{ this.item.genre }}</dd>
+          <dt>Duration</dt>
+          <dd>{{ this.item.length }}</dd>
+        </dl>
+      </div>
     </main>
     <main v-else-if="this.item.type === 'Newspaper'">
       <h1>Newspaper</h1>
       <h2>{{ this.item.periodicalTitle }}</h2>
       <hr />
-      <dl>
-        <dt>Id</dt>
-        <dd>{{ this.item.id }}</dd>
-        <dt>Title of the periodical</dt>
-        <dd>{{ this.item.periodicalTitle }}</dd>
-        <dt>Date</dt>
-        <dd>{{ this.item.date }}</dd>
-        <dt>Frequency</dt>
-        <dd>{{ this.item.frequency }}</dd>
-        <dt>Publication number</dt>
-        <dd>{{ this.item.number }}</dd>
-      </dl>
+      <div class="item-description-container">
+        <img src="../assets/newspaper.svg" alt="Newspaper" />
+        <dl>
+          <dt>Id</dt>
+          <dd>{{ this.item.id }}</dd>
+          <dt>Title of the periodical</dt>
+          <dd>{{ this.item.periodicalTitle }}</dd>
+          <dt>Date</dt>
+          <dd>{{ this.item.date }}</dd>
+          <dt>Frequency</dt>
+          <dd>{{ this.item.frequency }}</dd>
+          <dt>Publication number</dt>
+          <dd>{{ this.item.number }}</dd>
+        </dl>
+      </div>
     </main>
   </body>
 </template>
@@ -139,3 +154,13 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.item-description-container {
+  display: flex;
+}
+.item-description-container img {
+  padding: 20px;
+  width: 200px;
+}
+</style>
