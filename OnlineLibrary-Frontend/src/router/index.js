533a0f3aa7e5858ca1947b9b49b7d8e39429f84d
Gabriel Lacroix <lacroixgabriel@gmail.com>
2021-11-24 10:48:41 -0500

    Add browsing component to the router

--- OnlineLibrary-Frontend/src/router/index.js
+++ OnlineLibrary-Frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "@/components/Home.vue";
+import Browse from "@/components/Browse.vue";
 import Rooms from "@/components/Rooms.vue";
 import Room from "@/components/Room.vue";
 import NotFound from "@/components/NotFound.vue";
@@ -13,6 +14,11 @@ export default new Router({
       path: "/",
       name: "Home",
       component: Home
+    },
+    {
+      path: "/browse/",
+      name: "Browse",
+      component: Browse
     },
     {
       path: "/404/",
