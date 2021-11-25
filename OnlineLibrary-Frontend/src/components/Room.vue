95008239cfb28788d3e13ce312c032a1ccc7417b
Gabriel Lacroix <lacroixgabriel@gmail.com>
2021-11-25 13:35:59 -0500

    Fix prod (take 2)

--- OnlineLibrary-Frontend/src/components/Room.vue
+++ OnlineLibrary-Frontend/src/components/Room.vue
@@ -24,11 +24,11 @@ const config = require("../../config");
 
 const backendUrl =
   process.env.NODE_ENV === "production"
-    ? `http://${config.build.backendHost}:${config.build.backendPort}`
+    ? `http://${config.build.backendHost}`
     : `http://${config.dev.backendHost}:${config.dev.backendPort}`;
 const frontendUrl =
   process.env.NODE_ENV === "production"
-    ? `http://${config.build.host}:${config.build.port}`
+    ? `http://${config.build.host}`
     : `http://${config.dev.host}:${config.dev.port}`;
 
 const axios_instance = axios.create({
