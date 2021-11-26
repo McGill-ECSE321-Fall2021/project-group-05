ab49df5a79a2549f28722289729d38b38be7cc7d
Gabriel Lacroix <lacroixgabriel@gmail.com>
2021-11-26 18:30:36 -0500

    Add button to add a new movie copy

--- OnlineLibrary-Frontend/src/components/LibrarianItem.vue
+++ OnlineLibrary-Frontend/src/components/LibrarianItem.vue
@@ -308,6 +308,13 @@
           <td>{{ copy.status }}</td>
         </tr>
       </table>
+      <b-button @click="addCopy">Add copy</b-button>
+      <p class="success-message" v-if="addCopySuccessMessage">
+        {{ addCopySuccessMessage }}
+      </p>
+      <p class="error-message" v-if="addCopyErrorMessage">
+        {{ addCopyErrorMessage }}
+      </p>
     </main>
     <main v-else-if="this.item.type === 'Newspaper'">
       <h1>{{ this.item.periodicalTitle }}</h1>
