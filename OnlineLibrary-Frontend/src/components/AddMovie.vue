cb33561666ac37a6ecc408a0518746d239219100
SMI3 <sebastian_ionascu@hotmail.com>
2021-11-25 20:01:05 -0500

    took off comments in addbook.vue and created addmovie

--- OnlineLibrary-Frontend/src/components/AddMovie.vue
+++ OnlineLibrary-Frontend/src/components/AddMovie.vue
@@ -1,47 +1,13 @@
 <template>
   <div class="form-container">
-    <h2>Add a book</h2>
-    <b-form @submit="createBook">
+    <h2>Add a movie</h2>
+    <b-form @submit="createMovie">
       <b-form-group id="input-group-1" label="Title" label-for="input-1">
         <b-form-input
-          id="book-title"
+          id="movie-title"
           type="text"
           placeholder="Title"
-          v-model="bookTitle"
-          required
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group
-        id="input-group-2"
-        label="Number of pages"
-        label-for="input-2"
-      >
-        <b-form-input
-          id="book-number-of-pages"
-          type="number"
-          placeholder="Number of pages"
-          v-model="bookNumberOfPages"
-          required
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group id="input-group-3" label="Author" label-for="input-3">
-        <b-form-input
-          id="book-author"
-          type="text"
-          placeholder="Author"
-          v-model="bookAuthor"
-          required
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group id="input-group-4" label="ISBN" label-for="input-4">
-        <b-form-input
-          id="book-isbn"
-          type="number"
-          placeholder="ISBN"
-          v-model="bookIsbn"
+          v-model="movieTitle"
           required
         ></b-form-input>
       </b-form-group>
