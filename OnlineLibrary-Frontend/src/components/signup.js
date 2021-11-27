ae5c0e2a62477fdfa369e2885aafb3092f500f71
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2021-11-27 18:15:40 -0500

    Write scripting for signup component

--- OnlineLibrary-Frontend/src/components/signup.js
+++ OnlineLibrary-Frontend/src/components/signup.js
@@ -31,11 +31,71 @@ export default {
   methods: {
     submitInPersonMemberForm(event) {
       event.preventDefault();
-      console.log("In person member");
+      if (!this.isUsernameTaken()) {
+        this.createOnlineAccount();
+      }
+    },
+    isUsernameTaken() {
+      // TODO Add this once there's an endpoint for getting member by username
+      return false;
+    },
+    createOnlineAccount() {
+      const relativeUrl = '/member/' + this.memberId + '/onlineAccount';
+      const self = this;
+      AXIOS.post(relativeUrl, {
+        username: self.username,
+        emailAddress: self.emailAddress,
+        password: self.password
+      })
+        .then((response) => {
+          if (response.status === 200) {
+            sessionStorage.setItem("loggedInMember", JSON.stringify(response.data));
+            // Prevent 2 users from being logged in at once
+            sessionStorage.removeItem("loggedInLibrarian");
+            self.$router.push({ name: "MemberHome" });
+          }
+          // TODO: Do real exception handling once the backend returns useful errors
+          else {
+            self.memberErrorMsg = "Unable to sign up. Please double-check your library ID.";
+          }
+        })
+        // TODO: Do real exception handling once the backend returns useful errors
+        .catch((error) => {
+          self.memberErrorMsg = "Unable to sign up. Please double-check your library ID.";
+        });
     },
     submitNewMemberForm(event) {
       event.preventDefault();
-      console.log("New member");
+      if (!this.isUsernameTaken()) {
+        this.createNewMember();
+      }
+    },
+    createNewMember() {
+      const relativeUrl = '/member/';
+      const self = this;
+      AXIOS.post(relativeUrl, {
+        fullName: self.fullName,
+        address: self.physicalAddress,
+        isCitizen: false,
+        onlineAccount: {
+          username: self.username,
+          emailAddress: self.emailAddress,
+          password: self.password
+        }
+      })
+        .then((response) => {
+          if (response.status === 200) {
+            self.$router.push({ name: "Inactive" });
+          }
+          // TODO: Do real exception handling once the backend returns useful errors
+          else {
+            self.memberErrorMsg = "Unable to sign up. Please try again in a few minutes.";
+          }
+        })
+        // TODO: Do real exception handling once the backend returns useful errors
+        .catch((error) => {
+          self.memberErrorMsg = "Unable to sign up. Please try again in a few minutes.";
+        });
     }
   }
 }
