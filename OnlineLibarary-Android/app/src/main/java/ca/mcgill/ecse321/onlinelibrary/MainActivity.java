baafd05bf5101c9b3b4af80f7f6bdfa6ba4eed7f
Guillaume Delmas-Frenette <guillaume.delmas-frenette@mail.mcgill.ca>
2021-12-01 18:16:51 -0500

    Tried to setup the app

---
+++ OnlineLibarary-Android/app/src/main/java/ca/mcgill/ecse321/onlinelibrary/MainActivity.java
@@ -0,0 +1,20 @@
+package ca.mcgill.ecse321.onlinelibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("http://onlinelibrary-frontend-05a.herokuapp.com");
+    }
+}
